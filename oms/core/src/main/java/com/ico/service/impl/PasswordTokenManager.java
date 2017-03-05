96185feb493b6fccb97cbbde0f21c7d711a8185c
thilanka02 <thilanka.isurulakshan@gmail.com>
2017-03-05 22:05:07 +0530

    Project Commit

---
+++ oms/core/src/main/java/com/ico/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.ico.service.impl;
+
+import com.ico.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
